8018335f3da46f476a9bcffb5aca57ad5aa1c956
Aayushshah196 <aayushshah.kanu@docsumo.com>
2024-07-28 19:08:31 +0545

    Initialize queue package with dynamic discovery and imports

---
+++ newsllm/services/queue/__init__.py
@@ -0,0 +1,9 @@
+"""
+Author: Aayush Shah
+Description: Initialization file for the queue package. Sets up dynamic discovery and imports for queue classes.
+"""
+
+from newsllm.services.queue.base import AbstractQueue
+from newsllm.services.queue.factory import get_queue
+from newsllm.services.queue.list_queue import ListQueue
+from newsllm.services.queue.redis_queue import RedisQueue
